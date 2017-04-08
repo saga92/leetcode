f85aa89926ccb0022aa312b84101f2e6fd8883af
saga92 <yahao9210normal@126.com>
2017-04-09 01:16:09 +0800

    add lowest common ancestor of binary tree solution

---
+++ invert_binary_tree.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root){
+            invertTree(root->left);
+            invertTree(root->right);
+            std::swap(root->left,root->right);
+        }
+        return root;
+    }
+};
+
+int main(){
+    return 0;
+}
