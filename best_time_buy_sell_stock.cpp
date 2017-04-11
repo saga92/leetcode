0db3157a7fc40d98aa16968a837ca015880ce7ab
saga92 <yahao9210normal@126.com>
2017-04-11 23:22:25 +0800

    add best time buy sell stock solution

---
+++ best_time_buy_sell_stock.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include <limits.h>
+
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxPro=0, minPrice=INT_MAX;
+        for(int i=0;i<prices.size();++i){
+            minPrice=min(minPrice, prices[i]);
+            maxPro=max(maxPro, prices[i]-minPrice);
+        }
+        return maxPro;
+    }
+};
