f4a1f21aa17559135dd7115b9d829f49142b7298
saga92 <yahao9210normal@126.com>
2017-04-11 23:00:58 +0800

    add longest valid parentheses solution

--- longest_valid_parentheses.cpp
+++ longest_valid_parentheses.cpp
@@ -1,5 +1,6 @@
 #include<string>
 #include<stack>
+#include<vector>
 using namespace std;
 class Solution {
 public:
@@ -27,6 +28,28 @@ public:
             longest = max(longest, a);
         }
         return longest;
+    }
+
+    int longestValidParentheses_DP(string s) {
+        if(s.length() <= 1) return 0;
+        int curMax = 0;
+        vector<int> longest(s.size(),0);
+        for(int i=1; i < s.length(); i++){
+            if(s[i] == ')'){
+                if(s[i-1] == '('){
+                    longest[i] = (i-2) >= 0 ? (longest[i-2] + 2) : 2;
+                    curMax = max(longest[i],curMax);
+                }
+                else{ // if s[i-1] == ')', combine the previous length.
+                    if(i-longest[i-1]-1 >= 0 && s[i-longest[i-1]-1] == '('){
+                        longest[i] = longest[i-1] + 2 + ((i-longest[i-1]-2 >= 0)?longest[i-longest[i-1]-2]:0);
+                        curMax = max(longest[i],curMax);
+                    }
+                }
+            }
+            //else if s[i] == '(', skip it, because longest[i] must be 0
+        }
+        return curMax;
     }
 };
 
