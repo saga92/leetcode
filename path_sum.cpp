93fea0730f520c0673cc2e360139a0216a0ecf99
saga92 <yahao9210normal@126.com>
2017-04-19 17:52:26 +0800

    add solution

---
+++ path_sum.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+
+    }
+};
