42afc7f6f1ceedeeb52ebdd1c6b49e8625f4beb0
saga92 <yahao9210normal@126.com>
2017-04-20 10:10:18 +0800

    add solution

--- path_sum.cpp
+++ path_sum.cpp
@@ -9,6 +9,12 @@ struct TreeNode {
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-
+        if(!root) return false;
+        if(!root->left && !root->right){
+            if(sum==root->val) return true;
+            else return false;
+        }else{
+            return (hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val));
+        }
     }
 };
