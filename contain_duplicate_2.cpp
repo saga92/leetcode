7c91be29dd600526bde638c899eda0c8a1ccf3ce
saga92 <yahao9210normal@126.com>
2017-04-25 13:50:15 +0800

    add

---
+++ contain_duplicate_2.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        if(k<=0)
+            return false;
+        if(k>nums.size()-1)
+            k=nums.size()-1;
+        for(int i=0;i<nums.size();i++){
+            if(i>k)
+                s.erase(nums[i-k-1]);
+            if(s.find(nums[i])!=s.end())
+                return true;
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
