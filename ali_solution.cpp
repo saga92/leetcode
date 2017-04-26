b668813594c4d71dd9de8fb58c62ae864c01afe3
saga92 <8854570Aa>
2017-04-26 21:27:18 +0800

    add solution

--- ali_solution.cpp
+++ ali_solution.cpp
@@ -26,24 +26,35 @@ int main(){
     arr.insert(arr.end(), res);
     //cal
     int n=arr.size();
-    int x1=0,x2=0;
+    int x=0;
     int m=0;
     int p=0;
+    vector<int> ca;
     for(int i=0;(p=(m+mypow(2,i)))<=n;i++){
-        if(i&1){
-            //x2
-            for(int j=m;j<p;j++){
-                x2+=(arr[j]==-1?0:arr[j]);
-            }
-        }else{
-            //x1
-            for(int j=m;j<p;j++){
-                x1+=(arr[j]==-1?0:arr[j]);
-            }
+        for (int j = m; j < p; j++) {
+            x += (arr[j] == -1 ? 0 : arr[j]);
         }
+        ca.insert(ca.end(),x);
+        x=0;
         m+=mypow(2,i);
     }
-    cout<<std::max(x1,x2);
+    int* resArr=new int[ca.size()];
+    int max=0;
+    resArr[0]=ca[0];
+    if(ca.size()==1){
+        cout<<resArr[0];
+    }
+    if(ca.size()==2){
+        resArr[1]=std::max(ca[0], ca[1]);
+        cout<<resArr[1];
+    }
+    for(int i=2;i<ca.size();i++){
+        resArr[i]=std::max(resArr[i-1], resArr[i-2]+ca[i]);
+    }
+    for(int i=0;i<ca.size();i++){
+        max=std::max(resArr[i],max);
+    }
+    cout<<max;
     return 0;
 }
 
