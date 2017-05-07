3bbc0001dcdb8e6f60bcb555b0898e06dbb5dd32
saga92 <yahao9210normal@126.com>
2017-05-07 15:42:10 +0800

    add

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -1,4 +1,6 @@
 #include<string>
+#include <vector>
+
 using namespace std;
 class Solution {
 public:
@@ -6,16 +8,38 @@ public:
         if(s.empty()) return "";
         if(s.size()==1) return s;
         int min_start=0,max_len=1;
-        for(int i=0;i<s.size();){
-            if(s.size()-i<=max_len/2) break;
-            int j=i,k=i;
-
+        int slen=s.size();
+        vector<vector<int>> dp(slen, vector<int>(slen));
+        //init
+        for(int i=0;i<slen;i++){
+            for(int j=0;j<slen;j++){
+                if(i>=j){
+                    dp[i][j]=1;
+                }
+            }
         }
+        int i=0,j=0;
+        for(int k=1;k<s.size();k++){
+            for(i=0;(j=i+k)<s.size();i++){
+                if(s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1];
+                    if(dp[i][j]){
+                        if(k+1>max_len){
+                            min_start=i;
+                            max_len=k+1;
+                        }
+                    }
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return s.substr(min_start,max_len);
     }
 };
 
 int main(){
     Solution solution;
-    string res=solution.longestPalindrome("babad");
+    string res=solution.longestPalindrome("bb");
     return 0;
 }
